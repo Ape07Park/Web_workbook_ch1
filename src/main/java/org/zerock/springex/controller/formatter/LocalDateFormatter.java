a3d58bebf2e57fdc153a3c6cbfa2a18d7bb9d0c6
SUNGMIN PARK <ape072399@gmail.com>
2025-04-03 09:32:56 +0900

    주석 추가

--- src/main/java/org/zerock/springex/controller/formatter/LocalDateFormatter.java
+++ src/main/java/org/zerock/springex/controller/formatter/LocalDateFormatter.java
@@ -6,12 +6,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+// LocalDate 타입으로 변환시켜주는 포맷터
 public class LocalDateFormatter implements Formatter<LocalDate> {
+
+    // 문자열과 지역 정보가 들어오면 그 지역 대의 시간에 맞게 특정한 형식으로 변환해준다.
     @Override
     public LocalDate parse(String text, Locale locale) {
         return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
+    // 변환된 것을 출력시켜준다
     @Override
     public String print(LocalDate object, Locale locale) {
         return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(object);
